d7fb1ca18e43c5e89f2da945f1a8e8a669a79ac2
Piyush <46845991+Piyusshh@users.noreply.github.com>
2020-03-07 22:26:44 +0530

    1st part fully completed

--- main.c
+++ main.c
@@ -18,6 +18,14 @@ struct word_list_node
 };
 
 
+struct MRU_node
+{
+    char char_var[ws];
+    int freq;
+    struct MRU_node *next;
+};
+
+
 void print_full_dictionary(struct row_list_node *row) // to print full dictionary with all rows and coloumns...
 {
     struct row_list_node *r;
@@ -75,29 +83,6 @@ void create_row_list(struct row_list_node **row,char word[])
     struct row_list_node *r;
     r = *row;                                    // r will contain the value stored at address stored at row.
     int i;
-    for(i=0; i<26; i++)
-    {
-        struct row_list_node *temp;
-        temp = (struct row_list_node*)malloc(sizeof(struct row_list_node));
-        temp->char_var = temp_var;
-        temp_var++;                                // for increasing alphabets
-        temp->link_right = NULL;
-        temp->link_down = NULL;
-        if(*row == NULL)                           //check wheather the list is empty 
-        {
-            r = temp;
-            *row = r;
-        }
-        else
-        {
-            while(r->link_down != NULL)
-            {
-                r = r->link_down;
-            }
-            r->link_down = temp;
-        }
-        
-    }
 
     for(i=0; i<26; i++)
     {
@@ -122,6 +107,34 @@ void create_row_list(struct row_list_node **row,char word[])
         }
         
     }
+
+    i = 0;
+
+    for(i=0; i<26; i++)
+    {
+        struct row_list_node *temp;
+        temp = (struct row_list_node*)malloc(sizeof(struct row_list_node));
+        temp->char_var = temp_var;
+        temp_var++;                                // for increasing alphabets
+        temp->link_right = NULL;
+        temp->link_down = NULL;
+        if(*row == NULL)                           //check wheather the list is empty 
+        {
+            r = temp;
+            *row = r;
+        }
+        else
+        {
+            while(r->link_down != NULL)
+            {
+                r = r->link_down;
+            }
+            r->link_down = temp;
+        }
+        
+    }
+
+    
 }
 
 
@@ -202,6 +215,9 @@ void create_Dict (struct row_list_node *row, char word[])
 
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int length_of_word_list(struct word_list_node *word) //here the right link is passed.
 {
     int count = 0;
@@ -272,21 +288,231 @@ void bubble_sort_dictionary(struct row_list_node **row)
     }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void remove_punctuation(char str[])
+{
+    int i = 0;   // for old string
+    int j = 0;   // for new string
+    while(str[i] != '\0')
+    {
+        if((str[i]>='a' && str[i]<='z') || (str[i]>='a' && str[i]<='z'))
+        {
+            str[j] = str[i];
+            i++;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+        
+    }
+    str[j] = '\0'; 
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// function to find out middle element 
+struct word_list_node* middle(struct word_list_node* start, struct word_list_node* last) 
+{ 
+    if (start == NULL) 
+    {
+        return NULL;
+    }
+         
+    struct word_list_node* slow = start; 
+    struct word_list_node* fast = start -> next; 
+  
+    while (fast != last) 
+    { 
+        fast = fast -> next; 
+        if (fast != last) 
+        { 
+            slow = slow -> next; 
+            fast = fast -> next; 
+        } 
+    } 
+  
+    return slow; 
+}
 
 
 
+int binarySearch(struct word_list_node *head, char search_str[]) 
+{ 
+    struct word_list_node *start = head; 
+    struct word_list_node *last = NULL; 
+    int retval = 0;
+    int found = 0;
+  
+    while (last != start && found == 0)
+    { 
+        // Find middle 
+        struct word_list_node *mid = middle(start, last); 
+  
+        // If middle is empty 
+        if (strcmp(mid->word,search_str) != 0) 
+        {
+            retval = 0;   // not found
+        }
+             
+        // If value is present at middle 
+        if (strcmp(mid->word,search_str) == 0) 
+        {
+            retval =1;
+            found = 1;
+        }
+  
+        // If value is more than mid 
+        else if (strcmp(mid->word,search_str) > 0) 
+        {
+            last = mid;
+            
+        } 
+  
+        // If the value is less than mid. 
+        else
+        {
+            start = mid->next;
+        } 
+  
+    }  
+  
+    // value not present 
+    return retval; 
+}
+
+
+
+
+void search_in_dictionary_binary(struct row_list_node *row,char search_str[])
+{
+    struct row_list_node *r;
+    r = row;
+
+    int found;
+
+    while(r->char_var != search_str[0])
+    {
+        r = r->link_down;
+    }
+    //now i have recahed the correct word location
+    printf("The Index to be searched is %c\n",r->char_var);
+    found = binarySearch(r->link_right,search_str);
+    if (found == 1)
+    {
+        printf(" \"%s\" spellled correctly...........\n\n",search_str);
+    }
+    else if(found == 0)
+    {
+        printf(" \"%s\" spelled wrongly...........\n\n");
+    }
+    
+    
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int linear_search(struct word_list_node *link_right, char search_str[])
+{
+    int found = 0;
+    int retval;
+    struct word_list_node *temp;
+    temp = link_right;
+    while((temp != NULL)&&(found == 0))
+    {
+        if(strcmp(temp->word,search_str) == 0)
+        {
+            found = 1;
+            retval = 1;
+        }
+        else 
+        {
+            retval = 0;
+        }
+        temp = temp->next;
+
+    }
+
+    return retval;
+}
+
+
+void search_in_dictionary_linear(struct row_list_node *row,char search_str[])
+{
+    int found;
+    struct row_list_node *r;
+    r = row;
+
+    while(r->char_var != search_str[0])
+    {
+        r = r->link_down;
+    }
+    /*now i have recahed the correct word location*/
+    printf("The Index to be searched is %c\n",r->char_var);
+    
+    found = linear_search(r->link_right,search_str);
+    if (found == 1)
+    {
+        printf(" \"%s\" spellled correctly\n\n",search_str);
+    }
+    else
+    {
+        printf(" \"%s\" spelled wrongly\n\n");
+    }
+    
+    
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+int search_in_mru(struct MRU_node *mru, char str[])
+{
+    struct MRU_node *temp;
+    temp = mru;
+    int found = 0;
+    int retval = 0;
+    while(temp != NULL && (found == 0))
+    {
+        if(strcmp(temp->char_var,str) == 0)
+        {
+            found = 1;
+            retval = 1;
+
+        }
+        temp = temp->next;
+    }
+
+    return retval;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
     char str[ws];
+    char str1[ws];   // for reading from user input file.
+    struct MRU_node *mru = NULL;
+    char search_str[ws];
     struct row_list_node *row = NULL;
     char word[20];
 
     int choice;
     int flag = 1;
 
-    FILE *fp;
+    FILE *fp;   // for reading from dictionary file
+    FILE *fp1;  // for reading from user input
+    FILE *fp2;  // for writing into new file
+    FILE *fp3; // for reading from new file for searching.
+    FILE *fp4;
     char ch1[20];
-    fp = fopen("b.txt","r");
+    fp = fopen("dictionary.txt","r");
+    fp1 = fopen("input.txt","r");
+    
 
     
     printf("****************************************************Welcome**************************************************************\n");
@@ -298,6 +524,9 @@ int main()
         printf("3. Print Full Dictionary\n");
         printf("4. Create Full Dictionary\n");
         printf("5. Sort The Full Dictionary\n");
+        printf("6. Remove All punctuations from user input\n");
+        printf("7. Direct From Dictionary:- Search for the spelled and misspleed words\n");
+        printf("8. First from MRU:- Search for the speled and misspelled words\n");
         printf("10. Exit\n");
         scanf("%d",&choice);
         switch (choice)
@@ -318,13 +547,40 @@ int main()
                 
                     }
                     
-                    fclose(fp);
+                    // fclose(fp);
                     
             case 5: bubble_sort_dictionary(&row);
                     break;
 
+            case 6: fp2 = fopen("new_input.txt","w");  // after removing all the punctuations;
+                    while(fscanf(fp1,"%s",str1) != EOF)
+                    {
+                        remove_punctuation(str1);
+                        fprintf(fp2,"%s ",str1);
+                    } 
+                    fclose(fp2);
+                    break;
+
+            case 7: fp3 = fopen("new_input.txt","r");
+                    while(fscanf(fp3,"%s",search_str) != EOF)
+                    {
+                        search_in_dictionary_linear(row,search_str);
+                    }
+                    fclose(fp3);
+                    break;
+
+            case 8: fp4 = fopen("new_input.txt","r");
+                    while(fscanf(fp4,"%s",search_str) != EOF)
+                    {
+                        search_in_dictionary_binary(row,search_str);
+                    }
+                    fclose(fp4);
+                    break;
+
             case 10: printf("______________________________________Thank You______________________________________________________\n\n");
                     flag = 0;
+                    fclose(fp);
+                    fclose(fp1);
                     break;
             default: printf("Please Enter A Valid Choice\n\n\n"); 
                      break;   
